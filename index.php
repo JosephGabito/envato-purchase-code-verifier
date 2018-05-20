a52a118ebe2060bc88c6ce5d726c2e832cd5396b
Joseph Gabito <codehaiku.io@gmail.com>
2018-05-20 20:33:07 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sample Envato Purchase Code Verifier</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="wrap">
+		<!-- // Include the EnvatoPurchaseCodeVerifier.php file that holds the verifier class.--> 
+		<?php require_once 'EnvatoPurchaseCodeVerifier.php'; ?>
+		<!-- // Create your own access token at (https://build.envato.com/create-token)-->
+		<?php $access_token = 'your_token'; // Change 'your_token' value with your own token ?>
+		<!-- // Create new instance of EnvatoPurchaseCodeVerifier. -->
+		<!-- // Pass your own access token -->
+		<?php $purchase = new EnvatoPurchaseCodeVerifier($access_token); ?>
+
+		<?php $buyer_purchase_code = filter_input(INPUT_POST, 'purchase_code', FILTER_DEFAULT); ?>
+
+		<!-- check if user submits the form -->
+		<?php if ( ! empty( $buyer_purchase_code ) ) { ?>
+
+			<?php $verified = $purchase->verified($buyer_purchase_code); ?>
+			<!-- User purchase code is good!-->
+			<?php if ( $verified ) { ?>
+				<?php
+					 $item_id = $verified->item->id; 
+					 $item_name = $verified->item->name; 
+					 $buyer = $verified->buyer; 
+					 $license = $verified->license; 
+					 $amount = $verified->amount; 
+					 $sold_at = $verified->sold_at; 
+					 $supported_until = $verified->supported_until;
+				 ?>
+				 <h1>Valid Purchase Code</h1>
+				 <table>
+					<tr><td>Item ID:</td><td><?php echo $item_id; ?></td></tr>
+					<tr><td>Item Name:</td><td><?php echo $item_name; ?></td></tr>
+					<tr><td>Buyer:</td><td><?php echo $buyer; ?></td></tr>
+					<tr><td>License:</td><td><?php echo $license; ?></td></tr>
+					<tr><td>Amount:</td><td><?php echo $amount; ?></td></tr>
+					<tr><td>Sold At:</td><td><?php echo $sold_at; ?></td></tr>
+					<tr><td>Supported Until:</td><td><?php echo $supported_until; ?></td></tr>
+				</table>
+			<?php } else { ?>
+				<!-- Invalid purchase code -->
+				<h1>Invalid Purchase Code</h1>
+			<?php } ?>
+
+		<?php } else  { ?>
+			<form action="index.php" method="post">
+				<label>Purchase Code: </label>
+				<input type="text" name="purchase_code" placeholder="Type or paste the buyer's purchase code here"><br>
+				<input type="submit">
+			</form>
+		<?php } ?>
+
+		
+	</div>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,29 @@
+<h1>Envato Purchase Code Verifier</h1>
+<p>
+	Clone, download, or copy the 'EnvatoPurchaseCodeVerifier' nifty class to create your own Envato purchase verifier tool. You can go to index.php to see sample code. Read EnvatoPurchaseCodeVerifier.php to understand what the class is all about. It's really simple. 
+</p>
+<p>
+	Contributions are highly appreciated! 
+</p>
+<hr>
+<ol>
+<li>First, require the verifier class: 
+<pre>
+require_once 'EnvatoPurchaseCodeVerifier.php';
+</pre>
+</li>
+	
+<li>Next, use the code below to create an instance of 'EnvatoPurchaseCodeVerifier'. Create your own access token at: https://build.envato.com/create-token 
+<pre>
+$purchase = new EnvatoPurchaseCodeVerifier($access_token);
+</pre>
+</li>	
+<li>
+Then, check the user purchase code:
+<pre>
+$verified = $purchase->verified($buyer_purchase_code);
+// Will return false if purchase code is invalid, otherwise the purchase data.
+</pre>
+</li>
+</ol>
+<p>That's it! Happy Coding!</p>
